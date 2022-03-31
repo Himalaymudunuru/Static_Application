b7eba5bb0251a3d1f7be39232c0d09f9aa58fe4a
Himalaymudunuru <102711749+Himalaymudunuru@users.noreply.github.com>
2022-03-31 13:52:11 +0530

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('git clone') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Himalaymudunuru/Static_Application.git'
+            }
+        }
+        
+        stage('docker build') {
+            steps {
+               withCredentials([string(credentialsId: 'Dokcer_creds', variable: 'Password')]) {
+                   
+                   sh '''docker login -u=hima12345 -p="${Password}"
+                         docker build -t hima12345/nginx .
+                         docker push hima12345/nginx
+                   
+                   '''
+    
+                 }
+            }
+        }
+        
+         stage('deploy') {
+            steps {
+                withCredentials([kubeconfigContent(credentialsId: 'Kubeconfig', variable: 'KUBECONFIG_CONTENT')]) {
+                  sh 'echo "${KUBECONFIG_CONTENT}" >> admin.conf  && kubectl --kubeconfig ./admin.conf get nodes  && ls -l && pwd && cat deployment.yaml && kubectl apply -f $WORKSPCE/deployment.yaml --validate=false'
+                  
+               }
+            }
+        }
+    }
+}
